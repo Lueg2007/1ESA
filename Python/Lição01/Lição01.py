e87ca83c25729c5ec8e6c7839711c42a01493a61
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 11:32:33 -0300

    Lição1

---
+++ Python/Lição01/Lição01.py
@@ -0,0 +1,25 @@
+'''
+Ex1
+n1 = int(input("Digite um número:"))
+n2 = int(input("Coloque outro número:"))
+if n1 > n2:
+    print(f"{n1} é o maior número")
+else:
+    print(f"{n2} é o maior número")
+
+Ex2
+Ano = int(input("Digite seu ano de nascimento:"))
+if Ano <= 2009:
+    print("Pode votar!")
+else:
+    print("Não pode votar!")
+
+Ex3
+senha = int(input("Qual é a senha?"))
+
+if senha == 1234:
+ print("PERMITIDO")
+
+else:
+ print("NEGADO")
+'''
