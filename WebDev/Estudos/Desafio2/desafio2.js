a91643bfbf6667f56e1a983b368b49e5ecb70af8
Lueg2007 <lueg2007@gmail.com>
2025-05-18 23:07:06 -0300

    desafio

---
+++ WebDev/Estudos/Desafio2/desafio2.js
@@ -0,0 +1,36 @@
+//Ex1
+let dia = prompt('Qual é o dia da semana?');
+if (dia == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (dia == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+
+
+//Ex2
+let numero = prompt('Digite um número:')
+if (numero > 0) {
+    alert('Seu número é negativo')
+}else {
+    alert('Seu número é positivo')
+}
+
+
+//Ex3
+let pontuacao = 105;
+if (pontuacao >= 100) {
+    alert('Parabéns, você atingiu uma boa pontuação!');
+} else {
+    alert('A sua pontuação não foi muito boa, tente novamente :(');
+}
+
+
+//Ex4
+let saldo = 3000;
+alert(`Seu saldo é de ${saldo}`);
+
+//Ex5
+let nome = prompt('Insira o seu nome:');
+alert(`Seja muito bem vindo, ${nome}!`);
