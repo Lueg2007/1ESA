69d31970c2e7461593e150d69289cb1f95bcc047
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-27 09:47:22 -0300

    Aula10

---
+++ WebDev/Aula10/react_playground_ts-main/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Contador from './pages/contador/Contador';
+import Tarefa from './pages/tarefa/Tarefa';
+
+
+function App() {
+
+  return (
+    <>
+        <Tarefa></Tarefa>
+    </>
+  );
+}
+
+export default App;
---
+++ WebDev/Aula10/react_playground_ts-main/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+﻿
+interface homeProps {
+  titulo: string;
+  texto: string;
+}
+function Home(props: homeProps) {
+  return (
+    <div>
+      <h2>{props.titulo}</h2>
+    </div>
+  )
+}
+
+export default Home
